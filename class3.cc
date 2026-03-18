447ad16e4029e3fbe9853af956f8593e937eeb4c
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-03-17 21:28:50 -0300

    class 3 incomplete

---
+++ class3.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX_PILHA = 10;
+
+class PilhaInt { 
+  public:
+    PilhaInt();
+    PilhaInt( const PilhaInt& p );
+    virtual void empilha( int valor );
+    int desempilha();
+  private:
+    int tab[MAX_PILHA];
+    int atual;
+};
+
+PilhaInt::PilhaInt() {
+    cout << "Chamou o construtor" << endl;
+    for (int i=0; i<MAX_PILHA; i++){
+            tab[i] = 0;
+        }
+        atual = 0;
+};
+
+void PilhaInt::empilha(int valor) {tab[atual++] = valor;}
+
+int PilhaInt::desempilha () {
+    if (atual < 0) throw;
+    return tab[--atual];}
+
+int main() {
+
+    PilhaInt p;
+
+    for (int i=0; i<5; i++){ p.empilha(i); cout << "empilhou" << endl;}
+
+    while (true) { p.desempilha(); cout << "desempilhou" << endl;}
+
+    return 0;
+}
