813d20c0f75d6e4697bdc2b9dddf99c8ee8948a4
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-03-28 14:11:35 -0300

    beggining trabalho 1

---
+++ trabalhos/trab1.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sstream>
+
+/* PRIMEIRO place-holder: código da classe do aluno */
+
+using namespace std;
+int main() {
+  
+/* SEGUNDO place-holder: caso de teste */
+  
+  return 0;
+}
