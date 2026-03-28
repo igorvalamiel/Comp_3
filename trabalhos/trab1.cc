46b3c0b8265cf3cc1cfe76a0910a141373d0f7b0
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-03-28 15:24:37 -0300

    20/100
    1 ok

--- trabalhos/trab1.cc
+++ trabalhos/trab1.cc
@@ -1,12 +1,37 @@
 #include <iostream>
-#include <sstream>
-
-/* PRIMEIRO place-holder: código da classe do aluno */
+//#include <sstream>
 
 using namespace std;
+
+class PilhaInt {
+    public:
+        PilhaInt(): atual(0) {}
+
+        void empilha( const int valor ){
+            tab[atual++] = valor;}
+        
+        const int desempilha() {
+            if (atual < 0) throw;
+            return tab[--atual];}
+
+        void print( std::ostream& o, const char* msg="") {
+            o << msg << std::endl;}
+        
+        PilhaInt operator << (const int valor) {
+            empilha( valor );
+            return *this;}
+        
+    private:
+        static const int MAX_PILHA = 10;
+        int tab[MAX_PILHA];
+        int atual;
+};
+ 
 int main() {
-  
-/* SEGUNDO place-holder: caso de teste */
-  
-  return 0;
+    PilhaInt p;
+    p << 19 << 18 << 17 << 30;
+    p.print( cout );
+    cout << endl;
+
+    return 0;
 }
