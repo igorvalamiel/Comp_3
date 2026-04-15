57235065e79ba00d139e3217cf8ef8b5d9bb6419
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-04-15 20:52:41 -0300

    recomeçando

--- trabalhos/trab4.cc
+++ trabalhos/trab4.cc
@@ -7,36 +7,17 @@ class Pair {
 public:
   template <typename A, typename B>
   Pair( A a, B b ) {
-
+    // código aqui...
   }
-
-  string print (auto s1, auto s2) {
-    return s1 + " = " + s2 + "\n";
-  }
-
 private:
-  //AbstractPair *p;
-  
+  AbstractPair *p;
 };
 
-template <typename A, typename B>
 class AbstractPair {
-public:
-    AbstractPair (A a, B b){
-        s1 = a;
-        s2 = b;
-    }
-private:
-    A s1;
-    B s2;
-}
 
-void print( ostream& o, initializer_list<Pair> lista ) {
-    for (auto i : lista) {
-        string txt = i.print();
-        o << txt;
-    }
-}
+};
+
+void print( ostream& o, initializer_list<Pair> lista ) {}
 
 int main() {
  
