a9342540602f2b83d715343913e031c7d8c7685e
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-04-16 10:49:44 -0300

    n ta funcionando essa merda

--- trabalhos/trab5_apply.cc
+++ trabalhos/trab5_apply.cc
@@ -9,7 +9,18 @@ using namespace std;
 template <typename T>
 auto apply(vector<T> lista, auto F) {
     
-    auto Func(vector <T>) -> decltype(F);
+    auto tipo = decltype(F(lista[0]));
+    
+    cout << tipo << endl;
 
-    return Func(lista);
+    vector <tipo> res;
+    for (auto i : lista){
+        res.push_back(F(i))
+    }
+
+    return res;
 }
+
+//int main() {
+//    return 0;
+//}
