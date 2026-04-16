f5c8dda1578d7a44c102ec2a606d72bcc48213d6
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-04-15 21:57:48 -0300

    tentativa 1 (falha)

--- trabalhos/trab4.cc
+++ trabalhos/trab4.cc
@@ -3,21 +3,44 @@
 
 using namespace std;
 
-class Pair {
-public:
-  template <typename A, typename B>
-  Pair( A a, B b ) {
-    // código aqui...
-  }
-private:
-  AbstractPair *p;
-};
-
 class AbstractPair {
-
+  public:
+    auto imprime (auto x, auto y) {
+      return x << " = " << y;
+    }
+    virtual ~AbstractPair() {}
+  private:
 };
 
-void print( ostream& o, initializer_list<Pair> lista ) {}
+template <typename A, typename B>
+class ImplPair : public AbstractPair {
+  public:
+  private:
+    A a;
+    B b;
+};
+
+class Pair {
+  public:
+    template <typename A, typename B>
+    Pair( A a, B b ) {
+      valor = p->imprime(a, b);
+    }
+
+    string get_value() {
+      return valor;
+    }
+
+  private:
+    AbstractPair *p;
+    string valor;
+};
+
+void print( ostream& o, initializer_list<Pair> lista ) {
+  for (auto i : lista) {
+    o << i.get_value();
+  }
+}
 
 int main() {
  
