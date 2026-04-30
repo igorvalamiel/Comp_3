f8f0587407555bbfb91bd13dcefe242ce6b2358b
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-04-30 15:41:20 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- trabalhos/trab7.cc
+++ trabalhos/trab7.cc
@@ -2,21 +2,26 @@ using namespace std;
 
 class AbstractPair {
   public:
-    virtual void imprime (ostream& o) {}
-    virtual ~AbstractPair() {}
-  private:
+    virtual void imprime(ostream& o) const = 0;
+
+    virtual unique_ptr<AbstractPair> clone() const = 0;
+
+    virtual ~AbstractPair() = default;
 };
 
 template <typename A, typename B>
 class ImplPair : public AbstractPair {
   public:
-    ImplPair(A&& x, B&& y) : a(forward<A>(x)), b(forward<B>(y)) {}
+    ImplPair(const A& x, const B& y) : a(x), b(y) {}
 
-    virtual ~ImplPair() {}
-
-    void imprime (ostream& o) {
-      o << a << " = " << b << endl;    
+    void imprime(ostream& o) const override {
+      o << a << " = " << b << endl;
     }
+
+    unique_ptr<AbstractPair> clone() const override {
+      return make_unique<ImplPair<A, B>>(a, b);
+    }
+
   private:
     A a;
     B b;
@@ -25,19 +30,37 @@ class ImplPair : public AbstractPair {
 class Pair {
   public:
     template <typename A, typename B>
-    Pair(A&& a, B&& b) {
-      p = make_unique<ImplPair<A, B>>(forward<A>(a), forward<B>(b));
+    Pair(const A& a, const B& b) {
+      using DA = decay_t<A>;
+      using DB = decay_t<B>;
+      p = make_unique<ImplPair<DA, DB>>(a, b);
     }
 
+    Pair(const Pair& other) {
+      if (other.p) {
+        p = other.p->clone();
+      }
+    }
+
+    Pair& operator=(const Pair& other) {
+      if (this != &other) {
+        p = other.p ? other.p->clone() : nullptr;
+      }
+      return *this;
+    }
+
+    Pair(Pair&&) = default;
+    Pair& operator=(Pair&&) = default;
+
     void imprime_pair(ostream& o) const {
-      return p->imprime(o);
+      if (p) p->imprime(o);
     }
 
   private:
     unique_ptr<AbstractPair> p;
 };
 
-void print( ostream& o, initializer_list<Pair> lista ) {
+void print(ostream& o, initializer_list<Pair> lista) {
   for (const auto& i : lista) {
     i.imprime_pair(o);
   }
