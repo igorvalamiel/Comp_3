3eaf631a14242f1b636288555794d33ceae1fd6f
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-05-02 17:43:00 -0300

    100/100

--- trabalhos/trab8_stream.cc
+++ trabalhos/trab8_stream.cc
@@ -7,3 +7,5 @@ template <typename I, typename F>
 void operator | (I& item, F funcao) {
     for_each( begin(item), end(item),  funcao );
 }
+
+// deu bom fml!!! (essa foi namoral sem IA viva o Zimbrao)
