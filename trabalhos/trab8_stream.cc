9cb98d76d6501539a9ff20be57623d7a47bd908b
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-05-02 17:15:19 -0300

    começando a entender

--- trabalhos/trab8_stream.cc
+++ trabalhos/trab8_stream.cc
@@ -3,5 +3,5 @@ using namespace std;
 
 template <typename L1, typename L2>
 void operator | (L1 lado1, L2 lado2) {
-    for_each( tab, tab+10,  []( int x ) { cout << x*x << endl; } );
+    for_each( lado1, lado1+10,  lado2 );
 }
