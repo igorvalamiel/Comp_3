832b3c7a53077b3926666570da5bae6d2557fd56
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-05-05 19:48:39 -0300

    100/100
    finalmente foi essa m

--- trabalhos/trab7.cc
+++ trabalhos/trab7.cc
@@ -1,7 +1,8 @@
 using namespace std;
+
 class AbstractPair {
   public:
-    virtual void imprime (ostream& o) const {}
+    virtual void imprime (ostream& o) const = 0;
     virtual unique_ptr<AbstractPair> clone() const = 0;
     virtual ~AbstractPair() {}
   private:
@@ -10,19 +11,18 @@ class AbstractPair {
 template <typename A, typename B>
 class ImplPair : public AbstractPair {
   public:
+
     template <typename A2, typename B2>
     ImplPair(A2&& x, B2&& y) : a(forward<A2>(x)), b(forward<B2>(y)) {}
 
-    ImplPair(const A& x, const B& y) : a(x), b(y) {}
+    ImplPair(const ImplPair&) = default;
 
     void imprime (ostream& o) const override {
-      o << a << " = " << b << endl;    
+        o << a << " = " << b;
     }
 
     unique_ptr<AbstractPair> clone() const override {
-      auto tmpA = a;
-      auto tmpB = b;
-      return make_unique<ImplPair<decay_t<A>, decay_t<B>>>(tmpA, tmpB);
+        return make_unique<ImplPair<A, B>>(*this);
     }
 
   private:
@@ -30,35 +30,78 @@ class ImplPair : public AbstractPair {
     B b;
 };
 
+struct keepObject {
+    vector<unique_ptr<AbstractPair>> items;
+
+    ~keepObject() {
+        while (!items.empty()) {
+            items.pop_back();
+        }
+    }
+};
+
 class Pair {
   public:
     template <typename A, typename B>
     Pair(A&& a, B&& b) {
-        using DA = decay_t<A>;
-        using DB = decay_t<B>;
+        using AA = typename decay<A>::type;
+        using BB = typename decay<B>::type;
 
-        DA a_copy = forward<A>(a);
-        DB b_copy = forward<B>(b);
-
-        p = make_unique<ImplPair<DA, DB>>(a_copy, b_copy);
+        if constexpr (is_lvalue_reference<B>::value) {
+            keepList = get_pool();
+            auto temp = make_unique<ImplPair<AA, BB>>(forward<A>(a), b);
+            p = temp->clone();
+            keepList->items.push_back(move(temp));
+        } else {
+            p = make_unique<ImplPair<AA, BB>>(forward<A>(a), b);
+        }
     }
 
     Pair(const Pair& other) {
-        p = other.p->clone();
+        keepList = other.keepList;
+        if (other.p) p = other.p->clone();
     }
 
-    Pair(Pair&& other) = delete;
+    Pair& operator=(const Pair& other) {
+        if (this != &other) {
+            keepList = other.keepList;
+            p = other.p ? other.p->clone() : nullptr;
+        }
+
+        return *this;
+    }
+
+    Pair(Pair&&) = delete;
+    Pair& operator=(Pair&&) = delete;
 
     void imprime_pair(ostream& o) const {
         p->imprime(o);
     }
 
   private:
+    shared_ptr<keepObject> keepList;
     unique_ptr<AbstractPair> p;
+
+    static weak_ptr<keepObject>& current_pool() {
+        static weak_ptr<keepObject> wp;
+        return wp;
+    }
+
+    static shared_ptr<keepObject> get_pool() {
+        auto sp = current_pool().lock();
+
+        if (!sp) {
+            sp = make_shared<keepObject>();
+            current_pool() = sp;
+        }
+
+        return sp;
+    }
 };
 
-void print( ostream& o, const initializer_list<Pair>& lista ) {
+void print( ostream& o, initializer_list<Pair> lista ) {
   for (const auto& i : lista) {
     i.imprime_pair(o);
+    o << "\n";
   }
 }
--- trabalhos/trab7_completo.cc
+++ trabalhos/trab7_completo.cc
@@ -45,7 +45,7 @@ ostream& operator << ( ostream& o, const vector<T>& v ) {
 
 class AbstractPair {
   public:
-    virtual void imprime (ostream& o) const {}
+    virtual void imprime (ostream& o) const = 0;
     virtual unique_ptr<AbstractPair> clone() const = 0;
     virtual ~AbstractPair() {}
   private:
@@ -54,19 +54,18 @@ class AbstractPair {
 template <typename A, typename B>
 class ImplPair : public AbstractPair {
   public:
-    template <typename A2, typename B2>
-    ImplPair(A2&& x, B2&& y)
-        : a(forward<A2>(x)), b(forward<B2>(y)) {}
 
-    ImplPair(const A& x, const B& y)
-        : a(x), b(y) {}
+    template <typename A2, typename B2>
+    ImplPair(A2&& x, B2&& y) : a(forward<A2>(x)), b(forward<B2>(y)) {}
+
+    ImplPair(const ImplPair&) = default;
 
     void imprime (ostream& o) const override {
-        o << a << " = " << b << endl;
+        o << a << " = " << b;
     }
 
     unique_ptr<AbstractPair> clone() const override {
-        return make_unique<ImplPair<A, B>>(a, b);
+        return make_unique<ImplPair<A, B>>(*this);
     }
 
   private:
@@ -74,32 +73,79 @@ class ImplPair : public AbstractPair {
     B b;
 };
 
+struct keepObject {
+    vector<unique_ptr<AbstractPair>> items;
+
+    ~keepObject() {
+        while (!items.empty()) {
+            items.pop_back();
+        }
+    }
+};
+
 class Pair {
   public:
     template <typename A, typename B>
     Pair(A&& a, B&& b) {
-        p = make_unique<ImplPair<decay_t<A>, decay_t<B>>>(
-            forward<A>(a), forward<B>(b)
-        );
+        using AA = typename decay<A>::type;
+        using BB = typename decay<B>::type;
+
+        if constexpr (is_lvalue_reference<B>::value) {
+            keepList = get_pool();
+            auto temp = make_unique<ImplPair<AA, BB>>(forward<A>(a), b);
+            p = temp->clone();
+            keepList->items.push_back(move(temp));
+        } else {
+            p = make_unique<ImplPair<AA, BB>>(forward<A>(a), b);
+        }
     }
 
     Pair(const Pair& other) {
-        p = other.p->clone();
+        keepList = other.keepList;
+        if (other.p) p = other.p->clone();
     }
 
-    Pair(Pair&& other) = delete;
+    Pair& operator=(const Pair& other) {
+        if (this != &other) {
+            keepList = other.keepList;
+            p = other.p ? other.p->clone() : nullptr;
+        }
+
+        return *this;
+    }
+
+    Pair(Pair&&) = delete;
+    Pair& operator=(Pair&&) = delete;
 
     void imprime_pair(ostream& o) const {
         p->imprime(o);
     }
 
   private:
+    shared_ptr<keepObject> keepList;
     unique_ptr<AbstractPair> p;
+
+    static weak_ptr<keepObject>& current_pool() {
+        static weak_ptr<keepObject> wp;
+        return wp;
+    }
+
+    static shared_ptr<keepObject> get_pool() {
+        auto sp = current_pool().lock();
+
+        if (!sp) {
+            sp = make_shared<keepObject>();
+            current_pool() = sp;
+        }
+
+        return sp;
+    }
 };
 
-void print( ostream& o, const initializer_list<Pair>& lista ) {
+void print( ostream& o, initializer_list<Pair> lista ) {
   for (const auto& i : lista) {
     i.imprime_pair(o);
+    o << "\n";
   }
 }
 
