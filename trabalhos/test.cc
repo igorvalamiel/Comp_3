52f961b2f03aa7a0d8fc122898e467128027cf71
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-05-29 10:24:05 -0300

    90,91/100

--- trabalhos/test.cc
+++ trabalhos/test.cc
@@ -1,48 +1,56 @@
 #include <iostream>
-#include <algorithm>
-#include <array>
+#include <utility>
 
 using namespace std;
 
-// 1. Declaramos a classe base com um tipo padrão (I = void) para o segundo parâmetro
-template <typename F, typename I = void>
-class Bind {
-    public:
-        Bind(F f, I i) : Func(f), item(i) {}
+// Declaramos a classe Bind na frente para que o operator() possa conhecê-la
+template <typename F, typename... ArgsGuardados>
+class Bind;
 
-        template <typename... Args>
-        auto operator()(Args... args) {
-            return Func(item, args...);
-        }
-        
-    private:
-        F Func;
-        I item;
-};
-
-// 2. Especialização da classe para quando o segundo tipo for 'void' (apenas a função)
-template <typename F>
-class Bind<F, void> { // Indicamos aqui que esta é a versão específica para 'void'
-    public:
-        Bind(F f) : Func(f) {}
-
-        template <typename... Args>
-        auto operator()(Args... args) {
-            return Func(args...);
-        }
-        
-    private:
-        F Func;
-        // Removido o 'I item' porque aqui o tipo 'I' seria void (e não podemos ter variáveis void)
-};
-
-// 3. Suas funções auxiliares agora funcionam perfeitamente!
-template <typename F, typename I>
-Bind<F, I> bind(const F Func, const I item) {
-    return Bind<F, I>(Func, item);
+// Função auxiliar que cria o Bind de forma limpa
+template <typename F, typename... ArgsGuardados>
+Bind<F, ArgsGuardados...> bind_aux(F f, ArgsGuardados... args) {
+    return Bind<F, ArgsGuardados...>(f, args...);
 }
 
-template <typename F>
-Bind<F, void> bind(F Func) { // Especificamos 'void' aqui para casar com a especialização
-    return Bind<F, void>(Func);
+template <typename F, typename... ArgsGuardados>
+class Bind {
+public:
+    // Construtor que aceita a função e QUALQUER quantidade de argumentos guardados até agora
+    Bind(F f, ArgsGuardados... args) : Func(f), guardados(args...) {}
+
+    // O operador() mágico
+    template <typename... ArgsNovos>
+    auto operator()(ArgsNovos... args) {
+        // Se a função PODE ser chamada com os argumentos guardados + os novos:
+        if constexpr (requires { Func(std::get<ArgsGuardados>(guardados)..., args...); }) {
+            // Executa a função original
+            return chama_funcao(std::index_sequence_for<ArgsGuardados...>{}, args...);
+        } else {
+            // Caso contrário, NÃO executa. Cria um NOVO Bind acumulando os argumentos!
+            return junta_e_cria_novo_bind(std::index_sequence_for<ArgsGuardados...>{}, args...);
+        }
+    }
+
+private:
+    F Func;
+    std::tuple<ArgsGuardados...> guardados; // Guardamos os parâmetros em uma tupla
+
+    // Auxiliar 1: Desembrulha a tupla e executa a função original
+    template <size_t... Is, typename... ArgsNovos>
+    auto chama_funcao(std::index_sequence<Is...>, ArgsNovos... args) {
+        return Func(std::get<Is>(guardados)..., args...);
+    }
+
+    // Auxiliar 2: Desembrulha a tupla, junta com os novos argumentos e gera um novo Bind
+    template <size_t... Is, typename... ArgsNovos>
+    auto junta_e_cria_novo_bind(std::index_sequence<Is...>, ArgsNovos... args) {
+        return bind_aux(Func, std::get<Is>(guardados)..., args...);
+    }
+};
+
+// A função bind principal que o seu trabalho expõe
+template <typename F, typename... Args>
+auto bind(F Func, Args... args) {
+    return Bind<F, Args...>(Func, args...);
 }
--- trabalhos/trab14_bind.cc
+++ trabalhos/trab14_bind.cc
@@ -1,43 +1,46 @@
 #include <iostream>
+#include <utility>
 
 using namespace std;
 
-template <typename F, typename I = void>
-class Bind {
-    public:
-        Bind(F f, I i) : Func(f), item(i) {}
+template <typename F, typename... ArgsGuardados>
+class Bind;
 
-        template <typename... Args>
-        auto operator()(Args... args) {
-            return Func(item, args...);
-        }
-        
-    private:
-        F Func;
-        I item;
-};
-
-template <typename F>
-class Bind<F, void> {
-    public:
-
-        Bind(F f) : Func(f) {}
-
-        template <typename... Args>
-        auto operator()(Args... args) {
-            return Func(args...);
-        }
-        
-    private:
-        F Func;
-};
-
-template <typename F, typename I>
-Bind<F,I> bind(const F Func, const I item) {
-    return Bind<F,I>(Func, item);
+template <typename F, typename... ArgsGuardados>
+Bind<F, ArgsGuardados...> bind_aux(F f, ArgsGuardados... args) {
+    return Bind<F, ArgsGuardados...>(f, args...);
 }
 
-template <typename F>
-Bind<F> bind(F Func) {
-    return Bind<F>(Func);
+template <typename F, typename... ArgsGuardados>
+class Bind {
+public:
+    Bind(F f, ArgsGuardados... args) : Func(f), guardados(args...) {}
+
+    template <typename... ArgsNovos>
+    auto operator()(ArgsNovos... args) {
+        if constexpr (requires { Func(get<ArgsGuardados>(guardados)..., args...); }) {
+            return chama_funcao(index_sequence_for<ArgsGuardados...>{}, args...);
+        } else {
+            return junta_e_cria_novo_bind(index_sequence_for<ArgsGuardados...>{}, args...);
+        }
+    }
+
+private:
+    F Func;
+    tuple<ArgsGuardados...> guardados;
+
+    template <size_t... Is, typename... ArgsNovos>
+    auto chama_funcao(index_sequence<Is...>, ArgsNovos... args) {
+        return Func(get<Is>(guardados)..., args...);
+    }
+
+    template <size_t... Is, typename... ArgsNovos>
+    auto junta_e_cria_novo_bind(index_sequence<Is...>, ArgsNovos... args) {
+        return bind_aux(Func, get<Is>(guardados)..., args...);
+    }
+};
+
+template <typename F, typename... Args>
+auto bind(F Func, Args... args) {
+    return Bind<F, Args...>(Func, args...);
 }
