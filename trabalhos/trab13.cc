e6681e00ccc809de2208413f9585e3fa042e4daa
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-05-31 18:36:54 -0300

    12,5/100

--- trabalhos/trab13.cc
+++ trabalhos/trab13.cc
@@ -9,12 +9,12 @@
  
 using namespace std;
  
-//Var print( const Var& o ) {
-//    cout << "{ nome: " << o["nome"] << ", idade: " << o["idade"]( o ) << ", nascimento: " << o["nascimento"] << ", print: " << o["print"] << ", atr: " << o["atr"] << " }" << endl;
-//    return Var();   
-//}
-// 
-//void imprime( Var v ) { v["print"]( v ); }
+Var print( const Var& o ) {
+    cout << "{ nome: " << o["nome"] << ", idade: " << o["idade"]( o ) << ", nascimento: " << o["nascimento"] << ", print: " << o["print"] << ", atr: " << o["atr"] << " }" << endl;
+    return Var();   
+}
+ 
+void imprime( Var v ) { v["print"]( v ); }
  
 int main( int argc, char* argv[] ) try {
  
--- trabalhos/trab13_var.cc
+++ trabalhos/trab13_var.cc
@@ -5,10 +5,15 @@
 
 using namespace std;
 
+enum Tipo { T_UNDEFINED, T_INT, T_DOUBLE, T_STR, T_OBJ, T_FUNC };
+
 class Undefined {
     public:
+        Undefined(Tipo t = T_UNDEFINED) : t(t) {}
         virtual ~Undefined() = default;
         virtual void print(ostream& os) const {os << "undefined";}
+    private:
+        Tipo t;
 };
 
  
@@ -20,7 +25,7 @@ class Var {
         // Classes
         class Int: public Undefined {
             public:
-                Int( int n ): n(n) {}
+                Int( int n ): Undefined(T_INT), n(n) {}
                 virtual void print(ostream& os) const override {os << n;}
             private:
                 int n;
@@ -28,7 +33,7 @@ class Var {
 
         class Double: public Undefined {
             public:
-                Double( double d ): d(d) {}
+                Double( double d ): Undefined(T_DOUBLE), d(d) {}
                 virtual void print(ostream& os) const override {os << d;}
             private:
                 double d;
@@ -36,26 +41,37 @@ class Var {
         
         class String: public Undefined {
             public:
-                String( string s ): s(s) {}
+                String( string s ): Undefined(T_STR), s(s) {}
                 virtual void print(ostream& os) const override {os << s;}
             private:
                 string s;
         };
 
-        /*
-        class Function: public Undefined {
-            public:
-                Function() {}
-            private:
+        class Function : public Undefined {
+            public:                
+                Function(function<Var(Var)> f): Undefined(T_FUNC), func(f) {}
                 
+                virtual void print(ostream& os) const override { os << "[Function]"; }
+                
+                Var executar(Var arg) { return func(arg); }
+
+            private:
+                function<Var(Var)> func;   
         };
-        */
 
 
         class Object: public Undefined {
             public:
                 map<string, Var> atributos;
-                Object() {}
+                Object(): Undefined(T_OBJ) {}
+                virtual void print(ostream& os) const override {
+                    os << "{ ";
+                    for (auto it = atributos.begin(); it != atributos.end(); ++it) {
+                        os << "\"" << it->first << "\": " << it->second;
+                        if (next(it) != atributos.end()) os << ", ";
+                    }
+                    os << " }";
+                }
         };
 
 
@@ -82,9 +98,13 @@ class Var {
         //Var operator <= ( const Var& a, const Var& b ) { return !(b<a); }
         //Var operator >= ( const Var& a, const Var& b ) { return !(a<b); }
 
-        Var operator[] (string s) {
-            valor = shared_ptr<Undefined>( new Object());
-            return *this;
+        Var operator[](string s) const {
+            return Var();
+        }
+
+        Var operator()(Var arg) {
+            Function* fPtr = static_cast<Function*>(valor.get());
+            return fPtr->executar(arg);
         }
 
         friend ostream& operator<<(ostream& os, const Var& v) {
