9206c5f95f254ae13c0aac97f019e41d1f452394
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-06-12 16:04:33 -0300

    getting started

---
+++ trabalhos/trab16.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <string>
+#include <string.h>
+ 
+using namespace std;
+ 
+#include "trab16_lambda.cc"
+ 
+vector<int> operator + ( const vector<int>& v, int n ) {
+    vector<int> result{ v };
+    result.push_back( n );
+    return result;
+}
+ 
+int main() {
+    decltype(::x) x(::x);
+    
+    /* TESTCASE-PLACE-HOLDER */
+    vector< vector<int> > m{ { 1, 2, 3 }, { 0, 3, 7 }, { 1, 3 } };
+    m | []( auto v ) { return v[0] != 0; } | [x]( auto v ) { v + 7 | cout << x << ' '; };
+    
+    return 0;
+}
