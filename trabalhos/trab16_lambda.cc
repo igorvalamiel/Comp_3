ec000a713cf46d28487ec5b9cdde7d394ef19fb8
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-06-12 16:25:59 -0300

    75/100

--- trabalhos/trab16_lambda.cc
+++ trabalhos/trab16_lambda.cc
@@ -6,10 +6,42 @@
 
 using namespace std;
 
+class X;
+template <typename T> class Cte;
+template <typename Op1, typename Op2> class Soma;
+template <typename Op1, typename Op2> class Multiplica;
+template <typename Op1, typename Op2> class Modulo;
+template <typename Op1, typename Op2> class Igual;
+template <typename Op1, typename Op2> class Diferente;
+template <typename Left, typename Right> class print;
+template <typename Op1, typename Op2> class Pipe;
+template <typename Expr, typename IndexType> class Indexacao;
+
+template <typename T> struct is_my_expr : false_type {};
+template <> struct is_my_expr<X> : true_type {};
+template <typename A, typename B> struct is_my_expr<Soma<A, B>> : true_type {};
+template <typename A, typename B> struct is_my_expr<Multiplica<A, B>> : true_type {};
+template <typename A, typename B> struct is_my_expr<Modulo<A, B>> : true_type {};
+template <typename A, typename B> struct is_my_expr<Igual<A, B>> : true_type {};
+template <typename A, typename B> struct is_my_expr<Diferente<A, B>> : true_type {};
+template <typename T> struct is_my_expr<Cte<T>> : true_type {};
+template <typename L, typename R> struct is_my_expr<print<L, R>> : true_type {};
+template <typename A, typename B> struct is_my_expr<Pipe<A, B>> : true_type {};
+template <typename E, typename I> struct is_my_expr<Indexacao<E, I>> : true_type {};
+
+template <typename T>
+inline constexpr bool is_my_expr_v = is_my_expr<decay_t<T>>::value;
+
+template <typename T>
+auto wrap(T&& val);
+
 class X {
 public:
     template <typename K>
     auto operator()( K valor ) { return valor; }
+
+    template <typename T>
+    auto operator[](T&& index);
 };
 
 template <typename T>
@@ -46,11 +78,6 @@ private:
 };
 
 template <typename Op1, typename Op2>
-auto operator * ( Op1 a, Op2 b ) {
-    return Multiplica< Op1, Op2 >( a, b );
-}
-
-template <typename Op1, typename Op2>
 class Modulo {
 public:
     Modulo(Op1 a, Op2 b) : a(a), b(b) {}
@@ -72,7 +99,15 @@ private:
     Op1 a; Op2 b;
 };
 
-X x;
+template <typename Op1, typename Op2>
+class Diferente {
+public:
+    Diferente(Op1 a, Op2 b) : a(a), b(b) {}
+    template <typename K>
+    auto operator()(K valor) { return a(valor) != b(valor); }
+private:
+    Op1 a; Op2 b;
+};
 
 template <typename Left, typename Right>
 class print {
@@ -100,27 +135,28 @@ template <typename Op1, typename Op2>
 class Pipe {
 public:
     Pipe(Op1 a, Op2 b) : a(a), b(b) {}
-    
     template <typename K>
-    auto operator()(K&& valor) {
-        return a(forward<K>(valor)) | b;
-    }
+    auto operator()(K&& valor) { return a(forward<K>(valor)) | b; }
 private:
     Op1 a; Op2 b;
 };
 
-template <typename T> struct is_my_expr : false_type {};
-template <> struct is_my_expr<X> : true_type {};
-template <typename A, typename B> struct is_my_expr<Soma<A, B>> : true_type {};
-template <typename A, typename B> struct is_my_expr<Multiplica<A, B>> : true_type {};
-template <typename A, typename B> struct is_my_expr<Modulo<A, B>> : true_type {};
-template <typename A, typename B> struct is_my_expr<Igual<A, B>> : true_type {};
-template <typename T> struct is_my_expr<Cte<T>> : true_type {};
-template <typename L, typename R> struct is_my_expr<print<L, R>> : true_type {};
-template <typename A, typename B> struct is_my_expr<Pipe<A, B>> : true_type {};
+template <typename Expr, typename IndexType>
+class Indexacao {
+public:
+    Indexacao(Expr a, IndexType b) : a(a), b(b) {}
+    template <typename K>
+    auto operator()(K&& valor) {
+        return a(forward<K>(valor))[b(forward<K>(valor))];
+    }
+private:
+    Expr a; IndexType b;
+};
 
 template <typename T>
-inline constexpr bool is_my_expr_v = is_my_expr<decay_t<T>>::value;
+auto X::operator[](T&& index) {
+    return Indexacao<X, decltype(wrap(forward<T>(index)))>(*this, wrap(forward<T>(index)));
+}
 
 template <typename R, typename = enable_if_t<is_my_expr_v<R>>>
 auto operator<<(ostream& os, R&& right) {
@@ -134,7 +170,6 @@ auto operator<<(print<L, R> left, T&& right) {
 
 template<typename I, typename F>
 auto operator | (const I& item, F funcao) {
-
     using T = decay_t<decltype(*begin(item))>;
     using RetornoFuncao = invoke_result_t<F, T>;
 
@@ -170,22 +205,22 @@ auto wrap(T&& val) {
     }
 }
 
-template <typename Op1, typename Op2, typename = enable_if_t<is_my_expr_v<Op1> || is_my_expr_v<Op2>>>
-auto operator + ( Op1&& a, Op2&& b ) {
-    return Soma( wrap( forward<Op1>(a) ), wrap( forward<Op2>(b) ) );
-}
+template <typename Op1, typename Op2>
+auto operator * ( Op1 a, Op2 b ) { return Multiplica< Op1, Op2 >( a, b ); }
 
 template <typename Op1, typename Op2, typename = enable_if_t<is_my_expr_v<Op1> || is_my_expr_v<Op2>>>
-auto operator % (Op1&& a, Op2&& b) { 
-    return Modulo(wrap(forward<Op1>(a)), wrap(forward<Op2>(b))); 
-}
+auto operator + ( Op1&& a, Op2&& b ) { return Soma( wrap( forward<Op1>(a) ), wrap( forward<Op2>(b) ) ); }
 
 template <typename Op1, typename Op2, typename = enable_if_t<is_my_expr_v<Op1> || is_my_expr_v<Op2>>>
-auto operator == (Op1&& a, Op2&& b) { 
-    return Igual(wrap(forward<Op1>(a)), wrap(forward<Op2>(b))); 
-}
+auto operator % (Op1&& a, Op2&& b) { return Modulo(wrap(forward<Op1>(a)), wrap(forward<Op2>(b))); }
+
+template <typename Op1, typename Op2, typename = enable_if_t<is_my_expr_v<Op1> || is_my_expr_v<Op2>>>
+auto operator == (Op1&& a, Op2&& b) { return Igual(wrap(forward<Op1>(a)), wrap(forward<Op2>(b))); }
+
+template <typename Op1, typename Op2, typename = enable_if_t<is_my_expr_v<Op1> || is_my_expr_v<Op2>>>
+auto operator != (Op1&& a, Op2&& b) { return Diferente(wrap(forward<Op1>(a)), wrap(forward<Op2>(b))); }
 
 template <typename Op1, typename Op2, typename = enable_if_t<is_my_expr_v<Op1>>>
-auto operator | (Op1&& a, Op2&& b) {
-    return Pipe(forward<Op1>(a), forward<Op2>(b));
-}
+auto operator | (Op1&& a, Op2&& b) { return Pipe(forward<Op1>(a), forward<Op2>(b)); }
+
+X x;
